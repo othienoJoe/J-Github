3369eb94a13c3776170fa0db4ad774692063e0e3
othienoJoe <bryo094@gmail.com>
2021-08-30 11:46:09 +0300

    added content in the form component

--- src/app/github-form/github-form.component.ts
+++ src/app/github-form/github-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { GithubComponent } from '../github/github.component';
+import { GithubServiceService } from '../service/github-service.service';
 
 @Component({
   selector: 'app-github-form',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-form.component.css']
 })
 export class GithubFormComponent implements OnInit {
+  public profile: any;
+  public repos: any;
+  public username: any;
 
-  constructor() { }
+  constructor(private githubService: GithubServiceService) { }
+
+  findProfile() {
+    this.githubService.updateProfile(this.username);
+    this.githubService.getGithubInfo().subscribe(github => {
+      console.log(github);
+      this.profile = this.profile;
+    });
+
+    this.githubService.getGithubRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+  }
 
   ngOnInit(): void {
   }
--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubServiceService } from '../service/github-service.service';
+import { Component, Input, OnInit } from '@angular/core';
+// import { GithubServiceService } from '../service/github-service.service';
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
   styleUrls: ['./github.component.css']
 })
 export class GithubComponent implements OnInit {
-  profile: any;
-  repos: any;
-  username!: string;
+  // public profile: any;
+  // public repos: any;
+  // public username: any;
+  @Input()profile:any
 
-  constructor(private githubService: GithubServiceService) {
+  constructor() {
 
   }
 
-  findProfile() {
-    this.githubService.updateProfile(this.username);
-    this.githubService.getGithubInfo().subscribe(github => {
-      console.log(github);
-      this.profile = this.profile;
-    });
+  // findProfile() {
+  //   this.githubService.updateProfile(this.username);
+  //   this.githubService.getGithubInfo().subscribe(github => {
+  //     console.log(github);
+  //     this.profile = this.profile;
+  //   });
 
-    this.githubService.getGithubRepos().subscribe(repos => {
-      console.log(repos);
-      this.repos = repos;
-    })
-  }
+  //   this.githubService.getGithubRepos().subscribe(repos => {
+  //     console.log(repos);
+  //     this.repos = repos;
+  //   })
+  // }
 
   ngOnInit(): void {
   }
